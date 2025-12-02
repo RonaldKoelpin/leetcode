6aa4d82be0c575d1d5cdff557d7afb0c9d3592b1
RonaldKoelpin <ronald.koelpin@gmail.com>
2025-12-02 01:18:10 +0100

    implemented max points using vector geometry. slow but works.

--- maxPointsOnLine.py
+++ maxPointsOnLine.py
@@ -31,11 +31,46 @@ Constraints:
 """
 
 class Solution:
-    def create_line(self, x, y):
-        return None
+    def create_line(self, x, y) -> list[list[float], list[float]]:
+        starting_point = x
+        direction = []
+        for i in range(len(x)):
+            direction.append(y[i]-x[i])
+        return [starting_point, direction]
 
-    def is_point_on_line(self, p, x, y):
-        return None
+
+    def is_point_on_line(self, p, starting_point, direction) -> bool:
+        mu = set({})
+        for i in range(len(p)):
+            if direction[i] != 0:
+                mu.add((p[i]-starting_point[i])/direction[i])
+            elif p[i]-starting_point[i] == 0:
+                continue
+            else: return False
+        if len(mu) > 1:
+            return False
+        return True
 
     def max_points(self, points: list[list[int]]) -> int:
-        return None
+        n = len(points)
+        if n <= 2: return n
+        max_points_on_line = 0
+        for i in range(n-1):
+            for j in range(i+1, n, 1):
+                starting_point, direction = self.create_line(points[i], points[j])
+                points_on_line = 0
+                for k in range(n):
+                    if k == i or k == j:
+                        points_on_line += 1
+                    else:
+                        if self.is_point_on_line(points[k], starting_point, direction):
+                            points_on_line += 1
+                if points_on_line > max_points_on_line:
+                    max_points_on_line = points_on_line
+        return max_points_on_line
+
+if __name__ == "__main__":
+    points1 = [[1,1],[2,2],[3,3]]
+    points2 = [[1, 1], [3, 2], [5, 3], [4, 1], [2, 3], [1, 4]]
+    sol = Solution()
+    print(sol.max_points(points2))
